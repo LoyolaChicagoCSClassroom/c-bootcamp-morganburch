0ae8e286c734734aa4daf19af3f9ae9cfb49166b
morganburch <104595963+morganburch@users.noreply.github.com>
2024-02-13 18:40:39 +0000

    output token string fixed

--- R-FORTH/token/token.c
+++ R-FORTH/token/token.c
@@ -41,19 +41,20 @@ token_type_t classify_token(char *text){
     token_t* get_token_stream(FILE *stream){ 
         //array to be returned 
         token_t* tokens = malloc(MAX_TOKENS * sizeof(token_t));
-        int token_count = 0;
+        int token_count = 0; //to make sure we dont add too many tokens
 
         char *buffer = NULL; //hold the line of text read from the stream
         size_t buffsize = 0; //allocated by getline()
         char *typeName = "";
 
        while(getline(&buffer, &buffsize, stream) !=  -1){ //getline returns -1 when EOF
-        char *token = strtok(buffer, " ");//split stream at each blank space
+        char *token = strtok(buffer, " ");//split stream at each blank space -- delimiter
             while(token != NULL && token_count < MAX_TOKENS){ 
                 token_type_t type = classify_token(token);
+                char * tokenStr = token;
 
                 //add to array to be returned (with value and type)
-                while(token != NULL && token_count < MAX_TOKENS){ 
+                while((token != NULL) && (token_count < MAX_TOKENS)){ 
                     tokens[token_count].type = type;
                     tokens[token_count].text = strdup(token);  // Make sure to free this later!
 
@@ -68,7 +69,7 @@ token_type_t classify_token(char *text){
                 }else{ typeName = "Unknown";} 
 
                 
-                printf("Token: %s, Type: %s\n", token, typeName);
+                printf("Token: %s, Type: %s\n", tokenStr, typeName);
                 token = strtok(NULL, " ");//get next token
             }
        }
